b69dc62cd901f1e7adcb60c481ff42aa878245c6
Seif-Miehiar <Seif.Miehiar@gmail.com>
2021-06-22 11:59:25 +0300

    added search field

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,12 +1,27 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 
 const Header = () => {
 	return (
-		<Navbar collapseOnSelect sticky="top" expand="lg" variant="dark" bg="dark">
+		<Navbar sticky="top" expand="lg" variant="dark" bg="dark">
 			<Container>
 				<Navbar.Brand href="#">Wookiee</Navbar.Brand>
+				<Form className="d-flex">
+					<Button className="mr-2" variant="outline-success">
+						Search{" "}
+					</Button>
+
+					<FormControl
+						type="search"
+						placeholder="Search"
+						className="mr-2"
+						aria-label="Search"
+					/>
+				</Form>
 			</Container>
 		</Navbar>
 	);
