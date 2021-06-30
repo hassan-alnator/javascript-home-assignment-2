215b9368ad4f919d881a71f2d5d1522dc3e6d2d1
Seif-Miehiar <Seif.Miehiar@gmail.com>
2021-07-01 02:33:20 +0300

    updating app structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,57 @@
 import React, { useState, useEffect } from "react";
-import Movies from "./components/movies/Movies";
+// import Movies from "./components/movies/Movies";
 import Header from "./components/header/Header";
 import Genre from "./components/genre/Genre";
 import "./App.css";
 
 const App = () => {
-	const [movies, setMovies] = useState([]);
-	const [arrayOfGenre, setArrayOfGenre] = useState([]);
-	const axios = require("axios").default;
+	// const [movies, setMovies] = useState([]);
+	// const [arrayOfGenre, setArrayOfGenre] = useState([]);
+	// const axios = require("axios").default;
 
 	//useEffect works as componentDidMount in class components.
-	useEffect(() => {
-		//Using Axios to fetch data from server API passing authorization key in headers with the request.
-		axios
-			.get("https://wookie.codesubmit.io/movies", {
-				headers: {
-					Authorization: "Bearer Wookie2021",
-				},
-			})
-			//handling the request response if response was 202 ( accepted )
-			.then((response) => {
-				setMovies(response.data);
-				getGenre(response.data.movies);
-				// console.log(arrayOfGenre);
-			})
-			//handling server/request errors with catch block
-			.catch((error) => {
-				if (error) {
-					console.error(error);
-				}
-			});
-	}, []);
+	//!! Clean App.js
+	//Axios get movies.
+	//Search
+	//image-lazy-component || lazy load
 
-	const getGenre = (objectOfMovies) => {
-		// console.log(objectOfMovies);
-		if (objectOfMovies) {
-			objectOfMovies.map((element, index) => {
-				console.log(element);
-				if (!arrayOfGenre.includes(element)) {
-					arrayOfGenre.push(element.genres[0]);
-				}
-			});
-		}
-	};
+	// useEffect(() => {
+	// 	//Using Axios to fetch data from server API passing authorization key in headers with the request.
+	// 	axios
+	// 		.get("https://wookie.codesubmit.io/movies", {
+	// 			headers: {
+	// 				Authorization: "Bearer Wookie2021",
+	// 			},
+	// 		})
+	// 		//handling the request response if response was 202 ( accepted )
+	// 		.then((response) => {
+	// 			setMovies(response.data);
+	// 			// getGenre(response.data.movies);
+	// 			// console.log(arrayOfGenre);
+	// 		})
+	// 		//handling server/request errors with catch block
+	// 		.catch((error) => {
+	// 			if (error) {
+	// 				console.error(error);
+	// 			}
+	// 		});
+	// }, []);
+
+	// var filteringGenres = (oneObject) => {
+	// 	console.log(oneObject);
+	// 	if (oneObject.genres) {
+	// 		return oneObject.genres[0];
+	// 	}
+	// 	return false;
+	// };
+	// //use React state
+	// const getGenre = (objectOfMovies) => {
+	// 	if (objectOfMovies) {
+	// 		var x = objectOfMovies.filter(filteringGenres);
+	// 		console.log("objectOfMovies", x);
+	// 		setArrayOfGenre();
+	// 	}
+	// };
 
 	return (
 		<div>
